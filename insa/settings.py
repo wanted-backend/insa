199adad7f58b0a6c3a12b4f10e8565af55b1c961
pyjjin <pyheejin@gmail.com>
2020-05-25 16:17:16 +0900

    basic setting

--- insa/settings.py
+++ insa/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import config
+import pymysql
+
+pymysql.version_info = (1, 3, 13, "final", 0)
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
