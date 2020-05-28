972fb8fd46bbe3f0fd9fbd926dd9e986e2276d32
pyjjin <pyheejin@gmail.com>
2020-05-28 18:22:32 +0900

    signup 예외처리

--- user/views.py
+++ user/views.py
@@ -21,12 +21,12 @@ class SignUpView(View):
 			# 빈 문자열 검사
 			for value in data.values():
 				if value == '':
-					return HttpResponse(status=401)
+					return JsonResponse({'message':'입력 정보를 확인해주세요'}, status=401)
 
 			# 비밀번호 숫자, 영문 조합으로 6자리 이상인지 검증
 			for value, validator in self.validation.items():
 				if not validator(data[value]):
-					return HttpResponse(status=401)
+					return JsonResponse({'message':'영문자, 숫자만 사용하여 6자 이상 입력해주세요.'}, status=401)
 
 			User.objects.create(
 				email = data['email'],
@@ -36,7 +36,7 @@ class SignUpView(View):
 			)
 			return JsonResponse({'message':'SUCCESS'}, status=200)
 		except KeyError:
-			return JsonResponse({'message': 'INVALID_KEYS'}, status=401)
+			return JsonResponse({'message': 'INVALID KEYS'}, status=401)
 
 class LogInView(View):
 	def post(self, request):
