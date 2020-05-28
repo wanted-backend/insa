d29b19280c154e89a05f77901c3ed716a51a02f7
pyjjin <pyheejin@gmail.com>
2020-05-28 11:01:36 +0900

    model migration

---
+++ company/models.py
@@ -0,0 +1,240 @@
+from django.db import models
+
+class Company(models.Model):
+	location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
+	foundation_year = models.ForeignKey('Foundation_year', on_delete=models.SET_NULL, null=True)
+	employee = models.ForeignKey('Employee', on_delete=models.SET_NULL, null=True)
+	industry = models.ForeignKey('Industry', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=100)
+	address = models.CharField(max_length=500)
+	registration_number = models.IntegerField()
+	revenue = models.IntegerField()
+	description = models.TextField()
+	email = models.EmailField()
+	contact_number = models.CharField(max_length=50)
+	website = models.TextField(blank=True)
+	keyword = models.CharField(max_length=300, null=True)
+	recommender = models.CharField(max_length=100, null=True)
+	image_url = models.URLField(max_length=2000)
+	matchup = models.ManyToManyField('user.Matchup', through='user.Exception')
+	tag = models.ManyToManyField('Tag', through='Company_tag')
+	company_matchup_items = models.ManyToManyField('Matchup_item', through='Company_matchup_item')
+
+	class Meta:
+		db_table = 'companies'
+
+class Image(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	image_url = models.URLField(max_length=2000)
+
+	class Meta:
+		db_table = 'images'
+
+class Industry(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'industries'
+
+class Employee(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'employees'
+
+class Foundation_year(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'foundation_years'
+
+class Category(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'categories'
+
+class Tag(models.Model):
+	category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'tags'
+
+class Company_tag(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	tag = models.ForeignKey('Tag', on_delete=models.SET_NULL, null=True)
+
+	class Meta:
+		db_table = 'companies_tags'
+
+class Work(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=1000)
+	lat = models.DecimalField(max_digits=10, decimal_places=7)
+	lng = models.DecimalField(max_digits=10, decimal_places=7)
+
+	class Meta:
+		db_table = 'works'
+
+class Country(models.Model):
+	name = models.CharField(max_length=100)
+	number = models.CharField(max_length=30)
+	code = models.CharField(max_length=20)
+	currency = models.CharField(max_length=10)
+	english_currency = models.CharField(max_length=10)
+
+	class Meta:
+		db_table = 'countries'
+
+class Location(models.Model):
+	country = models.ForeignKey('Country', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'locations'
+
+class Position(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	theme = models.ForeignKey('Theme', on_delete=models.SET_NULL, null=True)
+	role = models.ForeignKey('Role', on_delete=models.SET_NULL, null=True)
+	work = models.ForeignKey('Work', on_delete=models.SET_NULL, null=True)
+	min_level = models.IntegerField(default=0)
+	max_level = models.IntegerField(default=0)
+	entry = models.BooleanField(default=0)
+	min_wage = models.IntegerField(default=0)
+	max_wage = models.IntegerField(default=0)
+	expiry_date = models.DateField(null=True)
+	always = models.BooleanField(default=0)
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+	responsibility = models.TextField()
+	qualification = models.TextField()
+	preferred = models.TextField(blank=True)
+	benifit = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	referrer = models.CharField(max_length=50)
+	volunteer = models.CharField(max_length=50)
+	total = models.CharField(max_length=50)
+	item = models.ManyToManyField('Item', through='Positions_item')
+
+	class Meta:
+		db_table = 'positions'
+
+class Bookmark(models.Model):
+	position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True)
+	user = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'bookmarks'
+
+class Theme(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.CharField(max_length=500)
+	image_url = models.URLField(max_length=2000)
+	inner_image_url = models.URLField(max_length=2000)
+	inner_description = models.TextField()
+
+	class Meta:
+		db_table = 'themes'
+
+class Item(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'items'
+
+class Positions_item(models.Model):
+	position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True)
+	item = models.ForeignKey('Item', on_delete=models.SET_NULL, null=True)
+	period = models.IntegerField(default=0)
+	expiration = models.BooleanField(default=0)
+	created_at = models.DateTimeField(auto_now_add=True)
+	used_at = models.DateField(null=True)
+	image_url = models.URLField(max_length=2000)
+	title = models.CharField(max_length=50)
+	description = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'positions_items'
+
+class Network(models.Model):
+	item = models.ForeignKey('Item', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=50)
+	period = models.IntegerField(default=0)
+	displayed_amount = models.IntegerField(default=0)
+	price_amount = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = 'networks'
+
+class Volunteers(models.Model):
+	position = models.ForeignKey('Position', on_delete=models.SET_NULL, null=True)
+	user = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'volunteers'
+
+class Like(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	user = models.ForeignKey('user.User', on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		db_table = 'like'
+
+class Job_category(models.Model):
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'job_categories'
+
+class Role(models.Model):
+	job_category = models.ForeignKey('Job_category', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'roles'
+
+class Companies_matchup(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	matchup = models.ForeignKey('user.Matchup', on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'companies_matchup'
+
+class Reading(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	matchup = models.ForeignKey('user.Matchup', on_delete=models.SET_NULL, null=True)
+	read = models.BooleanField(default=0)
+	interview = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'readings'
+
+class Matchup_item(models.Model):
+	name = models.CharField(max_length=50)
+	displayed_amount = models.IntegerField(default=0)
+	price_amount = models.IntegerField(default=0)
+	count = models.IntegerField(default=0)
+	days = models.IntegerField(default=0)
+
+	class Meta:
+		db_table = 'matchup_items'
+
+class Company_matchup_item(models.Model):
+	company = models.ForeignKey('Company', on_delete=models.SET_NULL, null=True)
+	matchup_item = models.ForeignKey('Matchup_item', on_delete=models.SET_NULL, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	expiration = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'companies_matchup_items'
+
---
+++ user/models.py
@@ -0,0 +1,177 @@
+from django.db import models
+
+class User(models.Model):
+	country = models.ForeignKey('company.Country', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=50)
+	email = models.EmailField(max_length=500)
+	password = models.CharField(max_length=500)
+	agreement = models.BooleanField(default=0)
+	contact = models.CharField(max_length=50)
+	image_url = models.URLField(max_length=2000)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	fail_count = models.IntegerField(default=0)
+	availavle = models.BooleanField(default=0)
+	position = models.CharField(max_length=100)
+	
+	class Meta:
+		db_table = 'users'
+
+class Security(models.Model):
+	user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	user_ip = models.CharField(max_length=100)
+	browser = models.CharField(max_length=100)
+	date = models.DateField(auto_now=True)
+	
+	class Meta:
+		db_table = 'security'
+
+class Matchup_career(models.Model):
+	year = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'matchup_careers'
+
+class Matchup(models.Model):
+	user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	role = models.ForeignKey('company.Role', on_delete=models.SET_NULL, null=True)
+	matchup_career = models.ForeignKey('Matchup_career', on_delete=models.SET_NULL, null=True)
+	country = models.ForeignKey('company.Country', on_delete=models.SET_NULL, null=True)
+	income = models.IntegerField(default=0)
+	school = models.CharField(max_length=100)
+	description = models.TextField()
+
+	class Meta:
+		db_table = 'matchup'
+
+class Exception(models.Model):
+	company = models.ForeignKey('company.Company', on_delete=models.SET_NULL, null=True)
+	matchup = models.ForeignKey('Matchup', on_delete=models.SET_NULL, null=True)
+
+	class Meta:
+		db_table = 'exceptions'
+
+class Matchup_skill(models.Model):
+	matchup = models.ForeignKey('Matchup', on_delete=models.SET_NULL, null=True)
+	skill = models.CharField(max_length=50)
+
+	class Meta:
+		db_table = 'matchup_skills'
+
+class Matchup_job(models.Model):
+	matchup = models.ForeignKey('Matchup', on_delete=models.SET_NULL, null=True)
+	job_text = models.ForeignKey('Job_text', on_delete=models.SET_NULL, null=True)
+
+	class Meta:
+		db_table = 'matchup_jobs'
+
+class Job_text(models.Model):
+	is_working = models.CharField(max_length=50)
+	text = models.CharField(max_length=50)
+	agreement = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'job_texts'
+
+class Work_information(models.Model):
+	matchup = models.ForeignKey('Matchup', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=100)
+	start = models.DateField()
+	end = models.DateField()
+	is_working = models.BooleanField(default=0)
+
+	class Meta:
+		db_table = 'work_informations'
+
+class Resume(models.Model):
+	user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.BooleanField(default=0)
+	is_matchup = models.BooleanField(default=0)
+	image_url = models.URLField(max_length=2000)
+
+	class Meta:
+		db_table = 'resumes'
+
+class Resume_file(models.Model):
+	user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+	resume_file = models.FilePathField(path='/user/resumes')
+	
+	class Meta:
+		db_table = 'resume_files'
+
+class Career(models.Model):
+	resume = models.ForeignKey('Resume', on_delete=models.SET_NULL, null=True)
+	start = models.DateField()
+	end = models.DateField()
+	is_working = models.BooleanField(default=0)
+	company = models.CharField(max_length=100)
+	position = models.CharField(max_length=100)
+	
+	class Meta:
+		db_table = 'careers'
+
+class Result(models.Model):
+	career = models.ForeignKey('Career', on_delete=models.SET_NULL, null=True)
+	start = models.DateField()
+	end = models.DateField()
+	title = models.CharField(max_length=300)
+	content = models.CharField(max_length=300)
+
+	class Meta:
+		db_table = 'results'
+
+class Education(models.Model):
+	career = models.ForeignKey('Career', on_delete=models.SET_NULL, null=True)
+	start = models.DateField()
+	end = models.DateField()
+	is_working = models.BooleanField(default=0)
+	school = models.CharField(max_length=100)
+	specialism = models.CharField(max_length=100)
+	subject = models.CharField(max_length=200)
+
+	class Meta:
+		db_table = 'educations'
+
+class Award(models.Model):
+	career = models.ForeignKey('Career', on_delete=models.SET_NULL, null=True)
+	date = models.DateField()
+	name = models.CharField(max_length=100)
+	content = models.CharField(max_length=200)
+
+	class Meta:
+		db_table = 'awards'
+
+class Language(models.Model):
+	career = models.ForeignKey('Career', on_delete=models.SET_NULL, null=True)
+	linguistic = models.ForeignKey('Linguistic', on_delete=models.SET_NULL, null=True)
+	level = models.ForeignKey('Level', on_delete=models.SET_NULL, null=True)
+
+	class Meta:
+		db_table = 'languages'
+
+class Linguistic(models.Model):
+	name = models.CharField(max_length=30)
+
+	class Meta:
+		db_table = 'linguistics'
+
+class Level(models.Model):
+	name = models.CharField(max_length=30)
+
+	class Meta:
+		db_table = 'levels'
+
+class Test(models.Model):
+	language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+	name = models.CharField(max_length=300)
+	score = models.CharField(max_length=150)
+	date = models.DateField()
+
+	class Meta:
+		db_table = 'tests'
+
+class Link(models.Model):
+	resume = models.ForeignKey('Resume', on_delete=models.SET_NULL, null=True)
+	url = models.URLField(max_length=2000)
