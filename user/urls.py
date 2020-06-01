ffb14604df9f16888d296d085af75472535498f8
pyjjin <pyheejin@gmail.com>
2020-06-01 21:16:08 +0900

    로그인, 회원가입 전에 이메일 검증 api 추가

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import UserRegisterView, AdminRegisterView, LogInView
+from .views import UserEmailExists, UserRegisterView, AdminRegisterView, LogInView
 
 urlpatterns = [
+    path('/extsis', UserEmailExists.as_view()),
 	path('/register', UserRegisterView.as_view()),
 	path('/adminregister', AdminRegisterView.as_view()),
 	path('/login', LogInView.as_view()),
