7e4db23d6cb1668728a8d89e15e8b1e5b011c861
pyjjin <pyheejin@gmail.com>
2020-06-01 10:20:49 +0900

    데이터베이스 업로드 파일 작성

--- database_file.py
+++ database_file.py
@@ -76,18 +76,17 @@ with open(CSV_PATH, newline='') as csvfile:
         Country.objects.create(
             name = row['name'],
             number = row['number'],
-            code = row['code'],
             currency = row['currency'],
             english_currency = row['english_currency']
         )
 
-CSV_PATH = './csv/locations.csv'
+CSV_PATH = './csv/cities.csv'
 with open(CSV_PATH, newline='') as csvfile:
     data_reader = csv.DictReader(csvfile)
 
     for row in data_reader:
         print(row)
-        Location.objects.create(
+        City.objects.create(
             country = Country.objects.get(id=row['countries_id']),
             name = row['name']
         )
@@ -99,18 +98,24 @@ with open(CSV_PATH, newline='') as csvfile:
     for row in data_reader:
         print(row)
         Company.objects.create(
-            location = Location.objects.get(id=row['locations_id']),
+            city = City.objects.get(id=row['cities_id']),
             foundation_year = Foundation_year.objects.get(id=row['foundation_years_id']),
             employee = Employee.objects.get(id=row['employees_id']),
             industry = Industry.objects.get(id=row['industries_id']),
             name = row['name'],
+            workplace = Workplace.objects.get(id=row['workplaces_id']),
             address = row['address'],
             registration_number = row['registration_number'],
             revenue = row['revenue'],
             description = row['description'],
             email = row['email'],
             contact_number = row['contact_number'],
-            website = row['website']
+            website = row['website'],
+            keyword = row['keyword'],
+            recommender = row['recommender'],
+            image_url = row['image_url'],
+            created_at = row['created_at'],
+            updated_at = row['updated_at']
         )
 
 CSV_PATH = './csv/tags.csv'
@@ -132,7 +137,7 @@ with open(CSV_PATH, newline='') as csvfile:
         print(row)
         Tag.objects.create(
             category = Category.objects.get(id=row['categories_id']),
-            name = row['name']
+            tag = Tag.objects.get(id=row['tags_id'])
         )
 
 CSV_PATH = './csv/images.csv'
@@ -146,14 +151,15 @@ with open(CSV_PATH, newline='') as csvfile:
             image_url = row['image_url']
         )
 
-CSV_PATH = './csv/works.csv'
+CSV_PATH = './csv/workplaces.csv'
 with open(CSV_PATH, newline='') as csvfile:
     data_reader = csv.DictReader(csvfile)
 
     for row in data_reader:
         print(row)
-        Work.objects.create(
+        Workplace.objects.create(
             company = Company.objects.get(id=row['companies_id']),
+            city = Company.objects.get(id=row['cities_id']),
             address = row['address'],
             lat = row['lat'],
             lng = row['lng']
@@ -165,7 +171,7 @@ with open(CSV_PATH, newline='') as csvfile:
 
     for row in data_reader:
         print(row)
-        Work.objects.create(
+        Workplace.objects.create(
             title = row['title'],
             description = row['address'],
             image_url = row['image_url'],
@@ -242,7 +248,6 @@ with open(CSV_PATH, newline='') as csvfile:
             company = Company.objects.get(id=row['companies_id']),
             theme = Theme.objects.get(id=row['themes_id']),
             role = Role.objects.get(id=row['roles_id']),
-            work = Work.objects.get(id=row['works_id']),
             min_level = row['min_level'],
             max_level = row['max_level'],
             entry = row['entry'],
@@ -260,8 +265,18 @@ with open(CSV_PATH, newline='') as csvfile:
             updated_at = row['updated_at'],
             referrer = row['referrer'],
             volunteer = row['volunteer'],
-            total = row['total'],
-            item = row['item']
+            total = row['total']
+        )
+
+CSV_PATH = './csv/positions_workplaces.csv'
+with open(CSV_PATH, newline='') as csvfile:
+    data_reader = csv.DictReader(csvfile)
+
+    for row in data_reader:
+        print(row)
+        Position_workplace.objects.create(
+            position = Position.objects.get(id=row['positions_id']),
+            workplace = Workplace.objects.get(id=row['workplaces_id'])
         )
 
 CSV_PATH = './csv/matchup_careers.csv'
