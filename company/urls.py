1f7671f802274da881095123ec212e4ac578ec1a
Ji <ji@Jiui-MacBookPro.local>
2020-06-02 10:04:05 +0900

    테마 리스트

--- company/urls.py
+++ company/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import CompanyRegister
+from .views import *
 
 urlpatterns = [
 	path('companyregister', CompanyRegister.as_view()),
+    path('/themetop/<int:theme_id>',ThemeTop.as_view()),
+    path('/themelist/<int:theme_id>',ThemeList.as_view()),
 ]
